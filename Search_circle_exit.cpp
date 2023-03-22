7f68858ab9e95ce49a13f0313e899ca21dff71ed
Colaris <claris9969100273@gmail.com>
2023-03-23 00:52:28 +0800

    first commit

---
+++ Search_circle_exit.cpp
@@ -0,0 +1,42 @@
+// APCS 2020/07/04
+// https://zerojudge.tw/ShowProblem?problemid=f581
+#include <iostream>
+#include <vector>
+
+
+int main()
+{
+    std::vector<int> arr_int;
+    std::vector<int> arr_sum_prev;
+    std::vector<int> mission;
+    int N = 6, M = 3, temp;
+    std::cin >> N >> M;
+    for (int i = 0; i < N; i++)
+    {
+        std::cin >> temp;
+        arr_int.push_back(temp);
+    }
+    arr_sum_prev.push_back(arr_int[0]);
+    for (int i = 1; i < N; i++)
+    {
+        temp = arr_int[i] + arr_sum_prev[i - 1];
+        arr_sum_prev.push_back(temp);
+    }
+    for (int i = 0; i < M; i++)
+    {
+        std::cin >> temp;
+        mission.push_back(temp);
+    }
+
+    int index = 0;
+    for (int i = 0; i < M; i++)
+    {
+        int target = (arr_sum_prev[index] - arr_int[index] + mission[i]) % *(arr_sum_prev.end() - 1);
+        if (!target)
+            target = arr_sum_prev[index] - arr_int[index] + mission[i];
+        index = std::lower_bound(arr_sum_prev.begin(), arr_sum_prev.end(), target) - arr_sum_prev.begin() + 1;
+    }
+
+    std::cout << index % N << std::endl;
+    return 0;
+}
